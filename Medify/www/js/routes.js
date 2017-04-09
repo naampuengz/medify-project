d2f0cb1ca88bdce039293680045df11f200e0322
Boonyapornn <naampuengz@gmail.com>
2017-04-09 20:09:41 +0700

    change file name

--- Medify/www/js/routes.js
+++ Medify/www/js/routes.js
@@ -8,71 +8,63 @@ angular.module('app.routes', [])
   // Each state's controller can be found in controllers.js
   $stateProvider
     
-  
-
-      .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
-  })
-
   .state('page5', {
     url: '/login_page',
-    templateUrl: 'templates/page5.html',
+    templateUrl: 'templates/login.html',
     controller: 'page5Ctrl'
   })
 
   .state('page6', {
     url: '/appoitment_summary_page',
-    templateUrl: 'templates/page6.html',
+    templateUrl: 'templates/appoitment_summary_page.html',
     controller: 'page6Ctrl'
   })
 
   .state('page7', {
     url: '/manage_medicine_page',
-    templateUrl: 'templates/page7.html',
+    templateUrl: 'templates/manage_medicine_page.html',
     controller: 'page7Ctrl'
   })
 
   .state('page8', {
     url: '/reister_page',
-    templateUrl: 'templates/page8.html',
+    templateUrl: 'templates/reister_page.html',
     controller: 'page8Ctrl'
   })
 
   .state('page10', {
     url: '/list_medicine_page',
-    templateUrl: 'templates/page10.html',
+    templateUrl: 'templates/list_medicine_page.html',
     controller: 'page10Ctrl'
   })
 
   .state('page9', {
     url: '/add_appointment_page',
-    templateUrl: 'templates/page9.html',
+    templateUrl: 'templates/add_appointment_page.html',
     controller: 'page9Ctrl'
   })
 
   .state('page11', {
     url: '/history_page',
-    templateUrl: 'templates/page11.html',
+    templateUrl: 'templates/history_page.html',
     controller: 'page11Ctrl'
   })
 
   .state('page12', {
     url: '/connect_device_page',
-    templateUrl: 'templates/page12.html',
+    templateUrl: 'templates/connect_device_page.html',
     controller: 'page12Ctrl'
   })
 
   .state('page13', {
     url: '/setting_page',
-    templateUrl: 'templates/page13.html',
+    templateUrl: 'templates/setting_page.html',
     controller: 'page13Ctrl'
   })
 
   .state('page15', {
     url: '/first_page',
-    templateUrl: 'templates/page15.html',
+    templateUrl: 'templates/first_page.html',
     controller: 'page15Ctrl'
   })
 
